f4073b7c0f02fc44cb5e4076f8bd63716dd77ca4
savagegeek22 <56052584+savagegeek22@users.noreply.github.com>
2025-03-11 08:13:18 -0400

    Update notify-rc.sh
    Updated lines to reflect what they were

--- notify-rc.sh
+++ notify-rc.sh
@@ -8,13 +8,14 @@
 #
 # Rocket.Chat webhook URL
 #
-# Old WebHook from Onomonopia
-# WEBHOOK_URL="https://onomonopia.savagegeek.net/hooks/644cbe70f3cf36b7511ae96d/TqD8cYgWHqXeyTTPrZSr235dkXccDmJhPNqrvz5c7DG7mYgp"
+# WebHook from Onomonopia
 
 WEBHOOK_URL="https://onomonopia.savagegeek.net/hooks/65af309a786b0b02c77e67ff/nwW5ijqrhy3nqLD6TLz6QGyFatPABtdmFQhTaYoLsvy9c89t"
 
 # Get the current date and time
 CURRENT_TIME=$(TZ="America/New_York" date +"%I:%M %p %m-%d-%Y")
+## Other ways to format it below
+
 #CURRENT_TIME=$(date -u +"%I:%M %p %m-%d-%Y" --timezone=America/New_York)
 #CURRENT_TIME=$(TZ="America/New_York" date -u +"%I:%M %p %m-%d-%Y")
 
@@ -22,12 +23,10 @@ CURRENT_TIME=$(TZ="America/New_York" date +"%I:%M %p %m-%d-%Y")
 # JSON payload for the message
 
 MESSAGE='{
-  "text": "\n\n \n\n:drpepper: *Check Your Chit!!* :medmari:\n\n*Time*: '$CURRENT_TIME'\n\n:point_right: [Monitoring Link](http://sg-centreon-01.savagegeek.com) :point_left: \n\n*Host*: '$1'\n\nService: '$4'\n\nAddress: '$7'\nHostState: '$2'\n\n\nServiceState: *'$5'*\n*Output*: '$6'\n",
+  "text": "\n\n \n\n:drpepper: *Check Your stuff!!* :medmari:\n\n*Time*: '$CURRENT_TIME'\n\n:point_right: [Monitoring Link](http://sg-centreon-01.savagegeek.com) :point_left: \n\n*Host*: '$1'\n\nService: '$4'\n\nAddress: '$7'\nHostState: '$2'\n\n\nServiceState: *'$5'*\n*Output*: '$6'\n",
   "username": "Centreon",
   "icon_url": "https://clipground.com/images/png-logos-1.png"
 }'
-
-
 
     curl -X POST -H "Content-Type: application/json" -d "$MESSAGE" "$WEBHOOK_URL"
 
